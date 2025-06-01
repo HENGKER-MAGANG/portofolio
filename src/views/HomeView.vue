20aa2812c253f60978b8a6d6ec910dca07d83fb3
Ikhsan <pratamaikhsan974.com>
2025-06-01 15:22:14 +0800

    testt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,9 +24,11 @@
           </path>
         </svg>Download Resume</button> -->
     </div>
-    <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="../../meeeee.jpg">
-    </div>
+   <div class="flex justify-center md:justify-start fadein-right">
+  <img alt="avatar" fetchpriority="high" decoding="async"
+    class="w-40 h-40 md:w-64 md:h-64 rounded-full border-4 border-amber-200 pict object-cover"
+    src="../../meeeee.jpg">
+</div>
   </main>
 </template>
 
